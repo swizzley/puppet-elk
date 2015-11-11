445a1d929d895059cb75f27ffa237d2b49843d10
Dustin Morgan <morgan@aspendenver.org>
2015-11-11 14:46:34 -0700

    vagrant provision

--- manifests/vagrant_provisioning.pp
+++ manifests/vagrant_provisioning.pp
@@ -7,15 +7,31 @@ $pvbkey_puppet = ''
 $pvtkey_hiera = ''
 $pubkey_hiera = ''
 
-#Git Deploy Key
+# Git Deploy Key
 $git_host = ''
 $git_username = ''
 $vagrant_start = 'exec{'initial puppet run': command => '/usr/bin/puppet apply -t /etc/puppet/manifests/site.pp'}'
 
-  file{'/etc/puppet/manifests/site.pp': 
+# Configure Vagrant
+$vagrant_home = '/root'
+
+  file{"${vagrant_home}": 
+    ensure => directory, 
+  }->
+  file{"${vagrant_home}/provision": 
+    ensure => directory, 
+    mode => '0644', 
+    content => template(elk/swizzley88-elk.pp.erb),
+  }->
+  file{"${vagrant_home}/provision/swizzley88-elk.pp": 
     ensure => present, 
     mode => '0644', 
     content => template(elk/swizzley88-elk.pp.erb),
+  }->
+  file{"${vagrant_home}/Vagrantfile": 
+    ensure => present, 
+    mode => '0644', 
+    source => 'puppet:///modules/elk/elk.rb',
   }
 
 }
