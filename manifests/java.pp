0c5c1679700103343e89bcf6bb24c6256a016feb
Dustin Morgan <morgan@aspendenver.org>
2015-11-12 12:55:59 -0700

    jre

--- manifests/java.pp
+++ manifests/java.pp
@@ -4,7 +4,7 @@ class elk::java {
   package { ['java-1.6.0-openjdk', 'java-1.7.0-openjdk']: ensure => absent }
 
   class { '::java':
-    distribution => 'jdk',
+    distribution => 'jre',
     version      => '7'
   }
 }
