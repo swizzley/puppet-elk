562da9adc6dceda4b6961d92a1420a55d9446452
Dustin Morgan <morgan@aspendenver.org>
2015-11-12 12:57:17 -0700

    ok

--- manifests/java.pp
+++ manifests/java.pp
@@ -1,8 +1,12 @@
 # class elk::java
 #
 class elk::java {
-  package { ['java-1.6.0-openjdk', 'java-1.7.0-openjdk']: ensure => absent }
-
+# This isn't working yet on puppetlabs-java
+#package { ['java-1.6.0-openjdk', 'java-1.7.0-openjdk']: ensure => absent }
+#  class { '::java':
+#    distribution => 'oracle-jdk',
+#    version      => '8'
+#  }
   class { '::java':
     distribution => 'jre',
     version      => '7'
