9759d09018a753e2a5718d67a75e795f5bd1f9f3
Dustin Morgan <morgan@aspendenver.org>
2015-11-12 12:53:04 -0700

    java 8

--- manifests/java.pp
+++ manifests/java.pp
@@ -4,7 +4,7 @@ class elk::java {
   package { ['java-1.6.0-openjdk', 'java-1.7.0-openjdk']: ensure => absent }
 
   class { '::java':
-    distribution => 'oracle-jre',
+    distribution => 'open-jdk',
     version      => '8'
   }
 }
