86560b59b91a058472b84b2ceeace8940e479d9f
Dustin Morgan <morgan@aspendenver.org>
2015-11-12 12:45:56 -0700

    hmm

--- manifests/kibana.pp
+++ manifests/kibana.pp
@@ -1,8 +1,10 @@
 # class elk::kibana
-class elk::kibana {
+class elk::kibana (){
   include elk::java
+  
   class { 'kibana': 
     base_url => $::elk::kibana_url,
     es_url => $::elk::es_url
   }
+  
 }
