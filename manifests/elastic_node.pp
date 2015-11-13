00b38a9cea75f73f7eb61e7a7dc600e8f488e07f
Dustin Morgan <morgan@aspendenver.org>
2015-11-12 23:08:08 -0700

    meh

--- manifests/elastic_node.pp
+++ manifests/elastic_node.pp
@@ -1,15 +1,20 @@
 # class elk::elastic_node
-class elk::elastic_node ($es_cluster = $::elk::es_cluster, $es_unicast_ip = $::elk::es_unicast_ip, $es_master = $::elk::params::es_master, $c10k = $::elk::params::c10k, $data_dir = $::elk::params::data_dir, $url_elasticsearch_plugin_hq = $::elk::params::url_elasticsearch_plugin_hq) inherits ::elk::params {
+class elk::elastic_node ($es_cluster = $::elk::es_cluster, 
+$es_unicast_ip = $::elk::es_unicast_ip, 
+$es_master = $::elk::params::es_master, 
+$c10k = $::elk::params::c10k, 
+$data_dir = $::elk::params::data_dir, 
+$url_elasticsearch_plugin_hq = $::elk::params::url_elasticsearch_plugin_hq) inherits ::elk::params {
   validate_string($es_unicast_ip)
   validate_array($es_cluster)
   validate_absolute_path($data_dir)
 
   case $::hostname {
-    "${master}" : {
+    "${es_master}" : {
       elk::elasticsearch { 'Elasticsearch':
         master  => true,
         data    => true,
-        cors    => false,
+        cors    => true,
         parity  => 0,
         unicast => $es_unicast_ip,
         path    => $data_dir,
@@ -19,9 +24,9 @@ class elk::elastic_node ($es_cluster = $::elk::es_cluster, $es_unicast_ip = $::e
     "${c10k}"   : {
       elk::elasticsearch { 'Elasticsearch':
         master  => false,
-        data    => false,
+        data    => true,
         cors    => true,
-        parity  => 0,
+        parity  => 1,
         unicast => $es_unicast_ip,
         path    => $data_dir,
         url_elasticsearch_plugin_hq => $url_elasticsearch_plugin_hq,
@@ -31,7 +36,7 @@ class elk::elastic_node ($es_cluster = $::elk::es_cluster, $es_unicast_ip = $::e
       elk::elasticsearch { 'Elasticsearch':
         master  => false,
         data    => true,
-        cors    => false,
+        cors    => true,
         parity  => 0,
         unicast => $es_unicast_ip,
         path    => $data_dir,
